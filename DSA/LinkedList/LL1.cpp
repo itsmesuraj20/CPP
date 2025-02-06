f5c0767ca822956246c955297827ab9f45abe8bf
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-02-07 00:02:57 +0530

    Linked List

--- DSA/LinkedList/LL1.cpp
+++ DSA/LinkedList/LL1.cpp
@@ -1,9 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
 
 int main(){
+
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+
+
+    Node *current = head;
+    while(current != NULL){
+        cout<<current->data<<endl;
+        current = current->next;
+    }
+
 
     return 0;
 }
